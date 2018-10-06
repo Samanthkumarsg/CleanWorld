138ee52ee5ce77bd19229f84ad1cd20d7caa3c76
Samanth kumar <43875028+Samanthkumarsg@users.noreply.github.com>
2018-10-06 10:37:53 +0530

    1

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>CleanWorld</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <script src="jquery-3.3.1.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <header></header>
+  </body>
+  </html>
