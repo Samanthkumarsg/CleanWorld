4cf0d01d08dca8338ed7e93db581b60c1a6f2ffe
Samanth kumar <43875028+Samanthkumarsg@users.noreply.github.com>
2018-10-06 11:32:59 +0530

    2

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <script src="jquery-3.3.1.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css"/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
+  <script src="script.js"></script>
   <body>
     <header></header>
+    <div class="container-fluid a">
+      <div class="row justify-content-center">
+      <div class="col-md-4 box" >
+        <form class="form-group" action="" method="post">
+          <input type="text" class="form-control" id="txt" placeholder="Where are you from ?"/>
+          <input type="text" class="form-control" id="txt" placeholder="Is your Place Clean ?"/>
+            <input type="text" class="form-control" id="txt" name="" placeholder="Where ?"/>
+          <button class="form-control btn btn-block " id="submit" type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+      </div>
+    </div>
   </body>
   </html>
