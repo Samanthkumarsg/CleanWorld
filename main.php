c3ddac6369bf8e53d7a99be13b0a3a14d5ed0c39
Samanth kumar <43875028+Samanthkumarsg@users.noreply.github.com>
2018-10-06 13:19:26 +0530

    change1

--- main.php
+++ main.php
@@ -12,7 +12,38 @@ else{
 
 function Insertdata($conn)
 {
-if(isset($_POST['submit'])) {echo"<script> alert('a');</script>";}
+if(isset($_POST['submit'])) {
+  $email = $_POST['email'];
+  $location = $_POST['location'];
+  $userlocation = $_POST['userlocation'];
+  $description = $_POST['description'];
+  $date = $_POST['date'];
+  $keys = "abc";
+
+  $name = $_FILES['upload']['name'];
+  $memory = $_FILES['upload']['size'];
+  $type = $_FILES['upload']['type'];
+  $tmp = $_FILES['upload']['tmp_name'];
+  $upload = 'uploads/'.$_FILES['upload']['name'];
+  $move = move_uploaded_file($tmp,$upload);
+  $size = $memory / 1024;
+  $sql = "INSERT INTO details(location, pictures, email, keys, userlocation, description,date) VALUES('$location', '$name',
+    '$email', '$keys', '$userlocation', '$description', '$date')";
+  $result = $conn->query($sql);
+  if($move == true)
+  {
+      echo "<h1 id='success'> Successfully uploaded  </h1>";
+ }
+    else
+    {
+      echo "<h1 id='success'> Successfully uploaded </h1> ";
+    }
+
+
+  }
+  else {
+
+  }
 }
 
 
